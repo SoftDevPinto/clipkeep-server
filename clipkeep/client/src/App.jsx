80f12e235d125a0251efd20f4c3df0c14e1c80a4
Chris Pinto <Chris.pinto1234@gmail.com>
2025-11-06 11:03:42 -0330

    updated url

--- clipkeep/client/src/App.jsx
+++ clipkeep/client/src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
     setLoading(true);
     setLog("Downloading video...");
     try {
-      const res = await fetch("http://localhost:5000/download", {
+      const res = await fetch("https://clipkeep-server.onrender.com/download", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url }),
